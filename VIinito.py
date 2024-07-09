db5e3f48fb42698495be1cde6ea15d87648b2fa0
Milinesa93 <45246940+Milinesa93@users.noreply.github.com>
2024-07-09 17:29:53 +0200

    archivo py

---
+++ VIinito.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statistics as stats
+import sklearn as sk
+
+df_wines = pd.read_csv("dfclean.csv")
